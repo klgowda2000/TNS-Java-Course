a120e1bb570c3cd0964ec2e899deeda30001a1a5
klgowda2000 <75177085+klgowda2000@users.noreply.github.com>
2023-03-03 19:43:58 +0530

    Add files via upload

---
+++ C2TC/day6/CodeFour.java
@@ -0,0 +1,13 @@
+package day6;
+
+public class CodeFour {
+	public static void main(String[] args) {
+//		int x =4;
+//		x = x+5;
+//		System.out.println(x);
+		
+		int a =20; //primitive data type
+		Integer i =a; //wrapper classa
+		System.out.println(i);
+	}
+}
---
+++ C2TC/day6/MultilevelInheritanceDemo.java
@@ -0,0 +1,18 @@
+package day6;
+
+public class MultilevelInheritanceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		City city = new City();
+		city.setCityName("Pune");
+		city.setArea(560001.0f);
+		city.setStateName("Maharashtra");
+		city.setLanguage("Hindi");
+		city.setCountryName("India");
+		city.setCapital("Mumbai");
+		System.out.println(city);
+
+	}
+
+}
